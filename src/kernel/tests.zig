117215c27aceccb57d76bf995e11d1628e3ed8c0
PierreLgol <plgol.perso@gmail.com>
2026-02-15 15:30:45 +0100

    test: add unified kernel test entrypoint

---
+++ src/kernel/tests.zig
@@ -0,0 +1,8 @@
+comptime {
+    _ = @import("main.zig");
+    _ = @import("arch.zig");
+    _ = @import("screens.zig");
+    _ = @import("keyboard.zig");
+    _ = @import("printk.zig");
+    _ = @import("lib/string.zig");
+}
